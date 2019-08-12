e84a0fb83f8450b30adb09646200a3fda7516a98
Aurélien Loyer <aur.loy@gmail.com>
2019-08-12 22:00:43 +0200

    feat: add inquirer to get project options 🤔

--- commands/create.js
+++ commands/create.js
@@ -1,47 +1,99 @@
 const fs = require('fs-extra');
 const util = require('util');
-const logging = require('../utils/logging');
-const readlineSync = require('readline-sync');
+const inquirer = require('inquirer');
 const rimraf = require('rimraf');
 const ncp = require('ncp').ncp;
 const ncpPromise = util.promisify(ncp);
 
-async function create(projectName, options) {
+const logging = require('../utils/logging');
+const writeFileTree = require('../utils/writeFileTree');
+
+async function create(directoryName, projectOptions) {
 	// TODO read options and use default or distant template like git repo ...
 
 	// create directory or replace it
-	if (projectName === '.') {
+	if (directoryName === '.') {
 		logging(undefined, 'Start create project in current directory: ', process.cwd());
+		const { isCurrentOk } = await inquirer.prompt({
+			name: 'isCurrentOk',
+			type: 'confirm',
+			message: `Generate project in current directory?`,
+		});
+		if (!isCurrentOk) {
+			return;
+		}
 	} else {
-		logging(undefined, 'Start create project with name: ', projectName);
+		logging(undefined, 'Start create project with name: ', directoryName);
 
-		if (fs.existsSync(projectName)) {
+		if (fs.existsSync(directoryName)) {
 			logging(undefined, 'Create project error: ', 'directory already exist');
-			const removeExisting = readlineSync.keyInYN('Remove this directory and create the project?');
-
+			const { removeExisting } = await inquirer.prompt({
+				name: 'removeExisting',
+				type: 'confirm',
+				message: `Remove this directory and create the project?`,
+			});
 			if (removeExisting === true) {
-				rimraf.sync(projectName);
-			} else if (removeExisting === false) {
+				rimraf.sync(directoryName);
+			} else {
 				console.log("Oh... It's ok... Choose another name 🤔");
 				process.exit(1);
 			}
 		}
 	}
-	fs.mkdir(projectName);
 
-	// generate project from template
-	ncpPromise(`${__dirname}/../template`, `${projectName}`).then(() => {
-		// replace all mustaches by prompt response
-		
-		// log commands to start
+	const options = await getProjectOptions({ directoryName });
 
-	}).catch(e => {
-		logging(undefined, 'Error during creating project: ', projectName);
-		console.log(e);
-		process.exit(1);
+	fs.mkdir(directoryName);
+
+	// store config into .spearker
+	await writeFileTree(`${directoryName}`, {
+		'.speaker': JSON.stringify(options),
 	});
 
+	// generate project from template
+	try {
+		await ncpPromise(`${__dirname}/../template`, `${directoryName}`);
+	} catch (error) {
+		logging(undefined, 'Error during creating project: ', directoryName);
+		console.log(error);
+		process.exit(1);
+	}
 
+	process.chdir(directoryName);
+
+	// replace all mustaches by prompt response
+	// log commands to start
+}
+
+async function getProjectOptions(options) {
+	return await inquirer.prompt([
+		{
+			name: 'projectName',
+			type: 'input',
+			default: options.directoryName,
+			message: `Confirm project name:`,
+		},
+		{
+			name: 'projectDescription',
+			type: 'input',
+			message: `Enter project description:`,
+		},
+		{
+			name: 'types',
+			type: 'checkbox',
+			message: `Pick all content needed:`,
+			choices: [
+				{ name: 'Codelab', value: 'codelab' },
+				{ name: 'Slides', value: 'slides' },
+				{ name: 'Other', value: 'other' },
+			],
+		},
+		{
+			name: 'gitinit',
+			type: 'confirm',
+			message: `Init git repository ?`,
+		},
+	]);
 }
 
 module.exports = (...args) => {
